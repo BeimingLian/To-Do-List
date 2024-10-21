52e7ab39252b3e59cc87b70579b67dd1f4b3c9e6
XiangLuo <57710409+XL10575@users.noreply.github.com>
2024-10-21 16:25:01 -0400

    bug fixed

--- src/main/java/org/demo/todo/service/usersService.java
+++ src/main/java/org/demo/todo/service/usersService.java
@@ -15,12 +15,5 @@ public interface usersService {
 
     void deleteUser(Long id);
 
-    public interface UserService {
-        users selectUserById(Long id);
-        List<users> selectAllUsers();
-        int insertUser(users user);
-        int updateUser(users user);
-        void deleteUser(Long id);
-    }
 
 }
